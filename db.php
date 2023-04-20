5ac2aa363b32e8f9908b0ed34fccc971907c1ef7
klevisbiraci <biraciklevi@gmail.com>
2023-04-20 17:01:25 +0200

    created a class to handle database

---
+++ db.php
@@ -0,0 +1,44 @@
+<?php
+
+class db{
+    public $con;
+    public $tableName = "files";
+
+    public function __construct($hostname,$username,$password,$dbName)
+    {
+        $this->con = new mysqli($hostname,$username,$password,$dbName);
+    }
+
+    public function insert($filename,$filesize,$filetype){
+        $time = time();
+        $date = date("Y-m-d H-i-s",$time);
+        $query = "INSERT INTO $this->tableName(filename,filesize,date,filetype)
+            VALUES('$filename','$filesize','$date','$filetype')";
+        $this->con->query($query); 
+    }
+
+    public function select(){
+        $query = "SELECT * FROM $this->tableName";
+        return $this->con->query($query); 
+    }
+
+    public function selectType($filename){
+        $typeReturn = null;
+        $query = "SELECT filetype FROM $this->tableName WHERE filename = '$filename'";
+        $fileType = $this->con->query($query);
+        $type = $fileType->fetch_all();
+        foreach($type as $data){
+            foreach($data as $returnData){
+                $typeReturn = $returnData;
+            }
+        }
+        return $typeReturn;
+    }
+
+    public function delete($filename){
+        $query = "DELETE FROM $this->tableName WHERE filename = '$filename'";
+        $this->con->query($query);
+    }
+}
+
+?>
